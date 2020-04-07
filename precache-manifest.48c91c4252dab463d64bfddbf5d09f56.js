3c4d02b2ad3979e585fed1b07e5f47fa423d5efb
TaAaAa7 <mostafa.ahmed.abdelfattah@gmail.com>
2020-04-07 09:15:00 +0000

    Updates

---
+++ precache-manifest.48c91c4252dab463d64bfddbf5d09f56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56fcd3694ef08e0eda6ab71086b1b344",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b4e457fad67aa9204287",
+    "url": "/monsters-rolodex/static/css/main.7a8862ab.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4e457fad67aa9204287",
+    "url": "/monsters-rolodex/static/js/main.7d2393a4.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
